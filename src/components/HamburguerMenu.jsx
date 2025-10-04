02fc204653c5adac6b59ed1b8b77654ee2d99f58
Kamiiuwu <medrano.9894276@gmail.com>
2025-10-04 02:31:13 -0400

    Styling Mobile Menu

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 const HamburguerMenu = () => {
 
@@ -10,9 +11,17 @@ const HamburguerMenu = () => {
     return (
         <div id="hamburger-menu" >
             <img src="/assets/icons/hamburger.svg" alt="Menu" onClick={toggleMenu} className="lg:hidden " />
-            <div className={`absolute lg:hidden right-0 top-0 h-dvh w-64 bg-black/90 backdrop-blur-sm shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
+            <div className={`absolute lg:hidden right-0 top-0 h-dvh w-[60%] bg-black/90 backdrop-blur-sm shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
                 <div className="flex justify-end p-4">
-                    <img src="/assets/icons/close.svg" alt="close" onClick={toggleMenu} className="hm-close lg: hidden bg-red-500" />
+                    <img src="/assets/icons/close.svg" alt="close" onClick={toggleMenu} className="hm-close-button" />
+                    <div>
+                        <ul className="nav-links-mobile">
+                            <NavLink to="/">Inicio</NavLink>
+                            <NavLink to="/info">Información</NavLink>
+                            <NavLink to="/reservations">Reservas</NavLink>
+                            <NavLink to="/system">Sistema</NavLink>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
